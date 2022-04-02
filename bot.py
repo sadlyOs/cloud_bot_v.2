1d15831fada25ea0a56b84beae46862409e967aa
Oleg Oleg <example@gmail.com>
2022-04-02 13:44:55 -0400

    relise

--- bot.py
+++ bot.py
@@ -13,11 +13,14 @@ import hashlib
 database = Database(host, dbname, user, password)
 
 
+
+
 @dp.message_handler(commands=['start'])
 async def add_user_id(msg: types.Message):
+    global choice
     id_user = msg.from_user.id
-    print(database.add_users_id(id_user))
     choice = create_choice()
+    print(database.add_users_id(id_user))
     a = database.return_catigories(id_user)
     if not a:
         await msg.answer(
@@ -30,16 +33,9 @@ async def add_user_id(msg: types.Message):
 @dp.callback_query_handler(text_contains='but1', state=None)
 async def get_photo_id(call: types.CallbackQuery):
     """ Проверяем нажатие первой кнопки """
-
     global id_
     await call.answer(cache_time=60)
     id_ = call.from_user.id  # Получаем id_user
-    a = database.return_catigories(id_)
-    if not a:
-        await call.answer(
-            """Добавьте одну категорию,чтобы появилась клавиатура,после добавление ещё раз нажмите старт\n
-Чтобы посмотреть как создать категорию нажмие на /info""")
-        return
     await bot.send_message(call.from_user.id,
                            "Выберите категорею, в которой вы сохраните фото",
                            reply_markup=add_key(id_))  # Выводим категории в виде кнопок
@@ -50,6 +46,7 @@ async def get_photo_id(call: types.CallbackQuery):
 @dp.callback_query_handler(lambda answ: answ.data in database.return_catigories(id_),
                            state=States.state1)  # Проверяем название нажатой кнопки в списке категорий
 async def answer(call: types.CallbackQuery, state: FSMContext):
+
     await call.answer(cache_time=60)
     catigori = call.data  # Сохраняем название кнопки
     await state.update_data(
@@ -62,7 +59,7 @@ async def answer(call: types.CallbackQuery, state: FSMContext):
 @dp.message_handler(content_types=['photo'], state=States.state2)
 async def save_photo(msg: types.Message, state: FSMContext):
     """Запрашиваем фото и получаем айди фото"""
-
+    choice = create_choice()
     data = await state.get_data()
     answer1 = data.get("answer1")  # Получаем данные из answer1
     answer2 = msg.photo[0].file_id  # Получаем айди фотки
@@ -72,6 +69,7 @@ async def save_photo(msg: types.Message, state: FSMContext):
                            database.add_id_photo(id_, answer1, keyid, hash_id_photo.hexdigest(),
                                                  answer2))  # Сохраняем айди,категорию и айди фотки
     await state.finish()  # Заканчиваем состояние
+    await msg.answer(text="Что делаем?", reply_markup=choice)
 
 
 @dp.callback_query_handler(text="buttton", state=None)
@@ -91,6 +89,7 @@ async def choice(call: types.CallbackQuery, state: FSMContext):
                            "Выберите категорею, в которой вы сохраните фото",
                            reply_markup=add_key(ids))
     await State_s.state1.set()
+    await call.message.edit_reply_markup(reply_markup=None)
 
 
 @dp.callback_query_handler(lambda answ: answ.data in database.return_catigories(ids),
@@ -100,8 +99,9 @@ async def answer(call: types.CallbackQuery, state: FSMContext):
     keyid = ids - 10000 * 10  # Указываем ключ
     list_photos = database.print_photos(ids, categories, keyid)  # Получаем возвращаемый список айди фотографий
     print(list_photos)
+    choice = create_choice()
     if list_photos == 0:
-        await bot.send_message(ids, "Такой категории не существует")  # Если вместо списка получили ложный вызов
+        await bot.send_message(ids, "У вас нет фотографий")  # Если вместо списка получили ложный вызов
     else:
         try:
             """Выводим все фотки с категории в альбомном виде"""
@@ -113,23 +113,28 @@ async def answer(call: types.CallbackQuery, state: FSMContext):
             await bot.send_message(ids, "Чтобы вывести фоток в категории должно быть более 1 фото")
     await state.finish()
 
+    await bot.send_message(ids, text="Что делаем?", reply_markup=choice)
+
 
 @dp.callback_query_handler(text_contains='del', state=None)
 async def choice_butt(call: types.CallbackQuery):
     """Проверяем нажатие второй кнопки и удаляем выбранную категорию"""
-
     global id_s
     await call.answer(cache_time=60)
     id_s = call.from_user.id
     await bot.send_message(id_s, "Выберите категорию для удаления", reply_markup=add_key(id_s))
     await State_del.state1.set()
+    await call.message.edit_reply_markup(reply_markup=None)
 
 
 @dp.callback_query_handler(lambda answ: answ.data in database.return_catigories(id_s), state=State_del.state1)
 async def cati_del(call: types.CallbackQuery, state: FSMContext):
+    await call.message.edit_reply_markup(reply_markup=None)
     categori = call.data
+    a = database.return_catigories(id_s)
     await call.answer(text=database.del_categories(id_s, categori), show_alert=True)
     await state.finish()
+
 
 
 @dp.message_handler(commands=['info'])
@@ -142,9 +147,11 @@ async def hash_check(msg: types.Message):
     """Создаём новую категорию"""
     id_user = msg.from_user.id
     message = msg.text
+    choice = create_choice()
     if message.startswith('#'):
         message = message.replace('#', '')
         await msg.answer(database.add_catigories(message, id_user))
+        await msg.answer(text="Что делаем?", reply_markup=choice)
     else:
         await msg.answer('Попробуйте /info')
 
--- db/main.py
+++ db/main.py
@@ -6,89 +6,83 @@ from db.password import host, dbname, user, password
 class Database:
     def __init__(self, host, dbname, user, password):
         self.conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
-        self.cursor = self.conn.cursor()
 
     def add_users_id(self, id_):
 
         """Сохраняем айди пользователей"""
-
-        self.cursor.execute(f"SELECT user_id FROM all_tg_users WHERE user_id = {id_}")
-        if self.cursor.fetchone() is None:
-            self.cursor.execute(f"INSERT INTO all_tg_users (user_id, user_count) VALUES ({id_}, {id_})")
-            return self.conn.commit()
-
-        return
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"SELECT user_id FROM all_tg_users WHERE user_id = {id_}")
+            if cursor.fetchone() is None:
+                cursor.execute(f"INSERT INTO all_tg_users (user_id, user_count) VALUES ({id_}, {id_})")
+                self.conn.commit()
 
     def add_catigories(self, catigories, id_):
 
         """Сохраняем айди пользователей и названия категорий"""
-
-        self.cursor.execute(
-            f"SELECT categories FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}'")
-        if self.cursor.fetchone() is None:
-            self.cursor.execute(f"INSERT INTO all_tg_users (user_id, categories) VALUES ({id_}, '{catigories}')")
-            self.conn.commit()
-            return f"Категория {catigories} успешна создана"
-        return "Данная категория уже существует"
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                f"SELECT categories FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}'")
+            if cursor.fetchone() is None:
+                cursor.execute(f"INSERT INTO all_tg_users (user_id, categories) VALUES ({id_}, '{catigories}')")
+                self.conn.commit()
+                return f"Категория {catigories} успешна создана"
+            return "Данная категория уже существует"
 
     def add_id_photo(self, id_, catigories, key_id, hash_photo, photo_id):
 
         """Сохраняем айди пользователей, названия категорий, айди фоток и распределяем их по именам категорий"""
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                f"SELECT categories FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}'")
+            if cursor.fetchone() is None:
+                return "У вас нет фотографий"
 
-        self.cursor.execute(
-            f"SELECT categories FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}'")
-        if self.cursor.fetchone() is None:
-            return "Такой категории не существует"
-
-        self.cursor.execute(
-            f"INSERT INTO all_tg_users (user_id, categories, key_id_photo, hash_id_photo, photo_id) VALUES ({id_}, '{catigories}', {key_id}, '{hash_photo}', '{photo_id}')")  # Добовляем хэш и ключ
-        self.conn.commit()
-        return f"Фотография сохранена в категорию '{catigories}'"
+            cursor.execute(
+                f"INSERT INTO all_tg_users (user_id, categories, key_id_photo, hash_id_photo, photo_id) VALUES ({id_}, '{catigories}', {key_id}, '{hash_photo}', '{photo_id}')")  # Добовляем хэш и ключ
+            self.conn.commit()
+            return f"Фотография сохранена в категорию '{catigories}'"
 
     def print_photos(self, id_, catigories, keyid):
 
         """Выводим все фото по именям категорий"""
-
-        self.cursor.execute(
-            f"SELECT categories FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}' AND key_id_photo = {keyid}")
-        if self.cursor.fetchone() is None:
-            return 0
-        list_photos = []
-        self.cursor.execute(f"SELECT photo_id FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}'")
-        for i in self.cursor.fetchall():
-            if i[0] is None:
-                continue
-            list_photos.append(i[0])
-        return list_photos
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                f"SELECT categories FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}' AND key_id_photo = {keyid}")
+            if cursor.fetchone() is None:
+                return 0
+            list_photos = []
+            cursor.execute(
+                f"SELECT photo_id FROM all_tg_users WHERE user_id = {id_} AND categories = '{catigories}'")
+            for i in cursor.fetchall():
+                if i[0] is None:
+                    continue
+                list_photos.append(i[0])
+            return list_photos
 
     def return_catigories(self, id_):
 
         """ Парсим категории в список list_categories и возвращаем его """
-
-        self.cursor.execute(
-            f"SELECT categories FROM all_tg_users WHERE user_id = {id_}")  # проверяем на существенность категорий
-        if self.cursor.fetchall() is None:
-            return
-        list_categories = []
-        self.cursor.execute(f"SELECT categories FROM all_tg_users WHERE user_id = {id_}")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            if row[0] is None:
-                continue  # Пропускаем добавление пустых значений
-            list_categories.append(row[0])
-        return list_categories
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                f"SELECT categories FROM all_tg_users WHERE user_id = {id_}")  # проверяем на существенность категорий
+            if cursor.fetchall() is None:
+                return
+            list_categories = []
+            cursor.execute(f"SELECT categories FROM all_tg_users WHERE user_id = {id_}")
+            rows = cursor.fetchall()
+            for row in rows:
+                if row[0] is None:
+                    continue  # Пропускаем добавление пустых значений
+                list_categories.append(row[0])
+            return list_categories
 
     def del_categories(self, id_, categories):
         """ Удаление категорий """
-
-        self.cursor.execute(
-            f"DELETE  FROM all_tg_users WHERE user_id = {id_} AND categories = '{categories}'")
-        self.conn.commit()
-        self.conn.close()
-        return f"Категория '{categories}' успешно удалена"
-
-    def close(self):
-        self.conn.close()
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                f"DELETE  FROM all_tg_users WHERE user_id = {id_} AND categories = '{categories}'")
+            self.conn.commit()
+            return f"Категория '{categories}' успешно удалена, теперь, чтобы снова появилась клавиатура добавьте категорию или нажмите на /start"
 
 
 '''
